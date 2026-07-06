9d90190ded0f1a7322dbbf1fd4de39ea81d75239
arslandaim-hub <arslandaim29@gmail.com>
2026-07-06 09:22:39 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.arslandaim.omegaplayer"
         minSdk = 25
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
